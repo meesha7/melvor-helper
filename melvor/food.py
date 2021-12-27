7148d0777a30b9614a82d567be10ae86800eb676
Meesha <44530786+meesha7@users.noreply.github.com>
2021-12-27 17:08:00 +0100

    Cap food heal at player's HP

--- melvor/food.py
+++ melvor/food.py
@@ -3,10 +3,12 @@ class Food:
         self.heal = heal
         self.amount = amount
 
+
 class AutoEat:
-    def __init__(self, tier, food):
+    def __init__(self, tier, food, player_hp=None):
         self.tier = tier
         self.food = food
+        self.player_hp = player_hp
 
         match self.tier:
             case 1:
@@ -17,7 +19,12 @@ class AutoEat:
                 self.efficiency = 100
 
     def one_heal(self):
-        return self.food.heal * self.efficiency / 100
+        one_heal_hp = self.food.heal * self.efficiency / 100
+
+        if self.player_hp < one_heal_hp:
+            return self.player_hp
+
+        return one_heal_hp
 
     def max_heal(self):
         return self.food.amount * self.one_heal()
